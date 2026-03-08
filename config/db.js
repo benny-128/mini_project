d3b7c82e2f20c8431243ba62323e12c3c6b3837f
benny-128 <gbharathkumar128@gmail.com>
2026-03-08 22:33:02 +0530

    database connection update

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'Bharath@!1125',
-  database:'library_db'
+const connection = mysql.createConnection({
+host: process.env.MYSQLHOST,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE,
+port: process.env.MYSQLPORT
 });
 
-db.connect((err)=>{
- if(err) throw err;
- console.log("MySQL Connected");
+connection.connect(function(err){
+if(err){
+console.log("Database connection failed:",err);
+}else{
+console.log("Railway MySQL Connected");
+}
 });
 
-module.exports = db;
+module.exports = connection;
